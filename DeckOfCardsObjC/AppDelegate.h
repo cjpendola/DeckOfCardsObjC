30d1da98e1451c6e8d77a01c15938b974b26697a
Carlos Pendola <carlospendola@carloss-macbook-pro.local>
2019-02-12 14:03:58 -0700

    Initial Commit

---
+++ DeckOfCardsObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeckOfCardsObjC
+//
+//  Created by Carlos Pendola on 2/12/19.
+//  Copyright © 2019 Carlos Pendola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeckOfCardsObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeckOfCardsObjC
+//
+//  Created by Carlos Pendola on 2/12/19.
+//  Copyright © 2019 Carlos Pendola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
